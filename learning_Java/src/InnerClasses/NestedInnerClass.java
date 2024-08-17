f92e115a8fe65f903a156cb4692bfe0db5c4544a
hytejasvi <hytejasvi@gmail.com>
2024-08-17 15:50:45 +0200

    nested Inner class

--- learning_Java/src/InnerClasses/NestedInnerClass.java
+++ learning_Java/src/InnerClasses/NestedInnerClass.java
@@ -1,28 +1,32 @@
 package InnerClasses;
 
-public class NestedInnerClass {
-    int x=10;
+class Outer {
+    int x = 10;
+
     class Inner {
         int y = 20;
 
-        void display() {
-            System.out.println("Inner display : X: "+x);
-            System.out.println("Inner display : Y: "+y);
+        void innerDisplay() {
+            System.out.println("Inner display : X: " + x);
+            System.out.println("Inner display : Y: " + y);
         }
     }
 
-    void display () {
+    void outerDisplay() {
         Inner in = new Inner();
-        in.display();
-        System.out.println("Printing in outer class using obj creation y: "+in.y);
-        System.out.println("Outer display : X: "+x);
-    }
-
-    public static void main (String[] args){
-        NestedInnerClass out = new NestedInnerClass();
-        System.out.print("calling form main: ");
-        out.display();
-        //NestedInnerClass.Inner i = new NestedInnerClass().new Inner();
-        //accessing the inner class from outside the outer class also
+        in.innerDisplay();
+        System.out.println("Printing in outer class using obj creation y: " + in.y);
+        System.out.println("Outer display : X: " + x);
     }
 }
+
+    public class NestedInnerClass {
+        public static void main(String[] args) {
+            Outer o = new Outer();//creating object of the outer class
+            o.outerDisplay(); //accessing methods of outer class
+            Outer.Inner inner = new Outer().new Inner();
+            //creating object of inner class --> first we have to create of outer class then the inner class
+            inner.innerDisplay();
+            //accessing the inner class from outside the outer class also
+        }
+    }
